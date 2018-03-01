87e900097b14402d0f095e9fd230ea8e7bb92de9
Dickson Sanchez Chinchay <d.sanchez.chinchay@gmail.com>
2018-03-01 17:19:58 -0500

    Add files via upload

---
+++ FuncionesControl2.m
@@ -0,0 +1,18 @@
+%% LRG FUNCION DE TRANSFERENCIA
+num = [0 0.3387 4.1089 12.4621];
+den=[1 6 8 0];
+rlocus(num,den)
+v= [-50 50 -50 50]
+grid
+
+%% RESPUESTA A ESCALON
+s =tf('s')
+gp=1/((s+2)*(s+4))
+gc=(0.3387*(s+6.0658)^2)/s   %controlador
+g=gc*gp
+h=1
+sys=feedback(g,h)
+t=0:0.1:10;
+step(sys,t)
+
+
